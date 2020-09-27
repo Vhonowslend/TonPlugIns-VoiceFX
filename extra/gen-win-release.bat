ab459088153c1c8158c4443e97d274e826428e98
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2020-09-27 08:02:20 +0200

    Version 0.2.1
    * Fixed install location being incorrect.
    * Attempted to fix desktop.ini being broken for no actual reason.

--- extra/gen-win-release.bat
+++ extra/gen-win-release.bat
@@ -5,9 +5,7 @@ SET "VSTSDK=C:\Tools\SDKs\Steinberg\VST_SDK"
 
 PUSHD "%_CWD%\.." >NUL
 cmake -H. -Bbuild/release/64 -G "Visual Studio 16 2019" -A"x64"   -DCMAKE_INSTALL_PREFIX="build/release/install" -DVSTSDK_DIR="%VSTSDK%" -DNVAFX_DIR="%NVAFX%"
-PAUSE
 cmake --build build/release/64 --config RelWithDebInfo --target INSTALL
-PAUSE
 cmake --build build/release/64 --config RelWithDebInfo --target PACKAGE_7Z
 cmake --build build/release/64 --config RelWithDebInfo --target PACKAGE_ZIP
 "build\release\64\installer.iss"
