49c369f45935cfbbe7062e3c22a126e836456cd2
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2021-05-20 02:48:43 +0200

    vst3: Improve initialization code

--- source/vst3.cpp
+++ source/vst3.cpp
@@ -21,20 +21,22 @@
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "vst3.hpp"
 #include <public.sdk/source/main/pluginfactory.h>
+
+#include "lib.hpp"
 #include "version.h"
+#include "vst3.hpp"
 #include "vst3_denoiser_controller.hpp"
 #include "vst3_denoiser_processor.hpp"
 
-#define D_LOG(MESSAGE, ...) voicefx::log("<VST2> " MESSAGE, __VA_ARGS__)
+#define D_LOG(MESSAGE, ...) voicefx::log("<VST3> " MESSAGE, __VA_ARGS__)
 
 BEGIN_FACTORY_DEF("Xaymar", "https://xaymar.com/", "mailto:info@xaymar.com")
 
 DEF_CLASS2(INLINE_UID_FROM_FUID(vst3::denoiser::processor_uid),
 		   PClassInfo::kManyInstances,       // Allow many instances
 		   kVstAudioEffectClass,             // Type
-		   "NVIDIA Voice Noise Removal",     // Name
+		   "VoiceFX",                        // Name
 		   Vst::kDistributable,              // Allow cross-computer usage.
 		   Vst::PlugType::kFxRestoration,    // Categories (separate with |)
 		   VERSION_STRING,                   // Version
@@ -42,14 +44,14 @@ DEF_CLASS2(INLINE_UID_FROM_FUID(vst3::denoiser::processor_uid),
 		   vst3::denoiser::processor::create // Function to create the instance.
 )
 DEF_CLASS2(INLINE_UID_FROM_FUID(vst3::denoiser::controller_uid),
-		   PClassInfo::kManyInstances,              // Allow many instances
-		   kVstComponentControllerClass,            // Type
-		   "NVIDIA Voice Noise Removal Controller", // Name
-		   0,                                       // Unused
-		   "",                                      // Unused
-		   VERSION_STRING,                          // Version
-		   kVstVersionString,                       // VST SDK Version
-		   vst3::denoiser::controller::create       // Function to create the instance.
+		   PClassInfo::kManyInstances,        // Allow many instances
+		   kVstComponentControllerClass,      // Type
+		   "VoiceFX Controller",              // Name
+		   0,                                 // Unused
+		   "",                                // Unused
+		   VERSION_STRING,                    // Version
+		   kVstVersionString,                 // VST SDK Version
+		   vst3::denoiser::controller::create // Function to create the instance.
 )
 
 END_FACTORY
@@ -59,13 +61,6 @@ bool InitModule()
 	try {
 		// Initialize VoiceFX library.
 		voicefx::initialize();
-		D_LOG("Initializing...");
-
-		// Adjust the path to the VST 3 plugin.
-		set_vst3_path(std::filesystem::path(gPath));
-
-		// Log that we are now ready to perform work.
-		D_LOG("Ready to work.");
 
 		// Return true to signal the VST 3 hosts that everything is fine.
 		return true;
