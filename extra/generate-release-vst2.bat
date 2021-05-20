a59c7ef9d189ca76386f1d37faf334f3c455ffb0
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2021-05-21 01:59:15 +0200

    extra: Don't generate VST3 code for VST2 binaries

--- extra/generate-release-vst2.bat
+++ extra/generate-release-vst2.bat
@@ -3,7 +3,7 @@ SET _CWD=%~dp0
 PUSHD "%_CWD%\.." >NUL
 
 :: VST2.x
-cmake -H. -Bbuild/release/vst2 -G "Visual Studio 16 2019" -A"x64" -DCMAKE_INSTALL_PREFIX="build/release/vst2/install" -DENABLE_STEINBERG_VST2=ON
+cmake -H. -Bbuild/release/vst2 -G "Visual Studio 16 2019" -A"x64" -DCMAKE_INSTALL_PREFIX="build/release/vst2/install" -DENABLE_STEINBERG_VST3=OFF -DENABLE_STEINBERG_VST2=ON
 cmake --build build/release/vst2 --config RelWithDebInfo --target INSTALL
 "build\release\vst2\installer-vst2.iss"
 
