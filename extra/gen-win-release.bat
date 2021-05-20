5cb4c5bf51f01e04e90c585a3a6b4d2f52325753
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2021-05-20 22:30:34 +0200

    extra: Fix build script

--- extra/gen-win-release.bat
+++ extra/gen-win-release.bat
@@ -1,10 +1,7 @@
 SET _CWD=%~dp0
 
-SET "NVAFX=C:\Tools\SDKs\Nvidia\Audio-Effects-0.5.3"
-SET "VSTSDK=C:\Tools\SDKs\Steinberg\VST_SDK"
-
 PUSHD "%_CWD%\.." >NUL
-cmake -H. -Bbuild/release/64 -G "Visual Studio 16 2019" -A"x64"   -DCMAKE_INSTALL_PREFIX="build/release/install" -DVSTSDK_DIR="%VSTSDK%" -DNVAFX_DIR="%NVAFX%"
+cmake -H. -Bbuild/release/64 -G "Visual Studio 16 2019" -A"x64" -DCMAKE_INSTALL_PREFIX="build/release/install" -DINSTALL_VST2=ON
 cmake --build build/release/64 --config RelWithDebInfo --target INSTALL
 cmake --build build/release/64 --config RelWithDebInfo --target PACKAGE_7Z
 cmake --build build/release/64 --config RelWithDebInfo --target PACKAGE_ZIP
