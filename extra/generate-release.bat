9ce0fe8b47b057651333ec3070d660a4f37c91dc
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2022-07-22 14:40:50 +0200

    extra: Generate all versions

--- extra/generate-release.bat
+++ extra/generate-release.bat
@@ -14,10 +14,13 @@ GOTO :EOF
         CALL :COMPILE_VST2
     ) ELSE IF "%~1"=="VST3" (
         CALL :COMPILE_VST3
+    ) ELSE IF "%~1"=="VST2_DEMO" (
+        CALL :COMPILE_VST2_DEMO
     ) ELSE IF "%~1"=="VST3_DEMO" (
         CALL :COMPILE_VST3_DEMO
     ) ELSE (
         START /D "%_CWD%" cmd /C generate-release.bat VST2
+        START /D "%_CWD%" cmd /C generate-release.bat VST2_DEMO
         START /D "%_CWD%" cmd /C generate-release.bat VST3
         START /D "%_CWD%" cmd /C generate-release.bat VST3_DEMO
     )
@@ -25,18 +28,23 @@ GOTO :EOF
 EXIT /B 0
 
 :COMPILE_VST2
-    CALL :CMAKE "vst2" -DENABLE_FULL_VERSION=ON -DENABLE_STEINBERG_VST3=OFF -DENABLE_STEINBERG_VST2=ON
-    "../build/release/vst2/installer-vst2.iss"    
+    CALL :CMAKE "vst2" -DENABLE_FULL_VERSION=ON -DENABLE_STEINBERG_VST3=OFF -DENABLE_STEINBERG_VST2=ON -DPACKAGE_PREFIX="../build/release/"
+    "%ProgramFiles(x86)%\Inno Setup 6\ISCC" ../build/release/vst2/installer-vst2.iss"    
+EXIT /B 0
+
+:COMPILE_VST2_DEMO
+    CALL :CMAKE "vst2d" -DENABLE_FULL_VERSION=OFF -DENABLE_STEINBERG_VST3=OFF -DENABLE_STEINBERG_VST2=ON -DPACKAGE_PREFIX="../build/release/"
+    "%ProgramFiles(x86)%\Inno Setup 6\ISCC" "../build/release/vst2d/installer-vst2.iss"    
 EXIT /B 0
 
 :COMPILE_VST3
-    CALL :CMAKE "vst3" -DENABLE_FULL_VERSION=ON -DENABLE_STEINBERG_VST3=ON -DENABLE_STEINBERG_VST2=OFF
-    "../build/release/vst3/installer-vst3.iss"
+    CALL :CMAKE "vst3" -DENABLE_FULL_VERSION=ON -DENABLE_STEINBERG_VST3=ON -DENABLE_STEINBERG_VST2=OFF -DPACKAGE_PREFIX="../build/release/"
+    "%ProgramFiles(x86)%\Inno Setup 6\ISCC" "../build/release/vst3/installer-vst3.iss"
 EXIT /B 0
 
 :COMPILE_VST3_DEMO
-    CALL :CMAKE "vst3d" -DENABLE_FULL_VERSION=OFF -DENABLE_STEINBERG_VST3=ON -DENABLE_STEINBERG_VST2=OFF
-    "../build/release/vst3d/installer-vst3.iss"
+    CALL :CMAKE "vst3d" -DENABLE_FULL_VERSION=OFF -DENABLE_STEINBERG_VST3=ON -DENABLE_STEINBERG_VST2=OFF -DPACKAGE_PREFIX="../build/release/"
+    "%ProgramFiles(x86)%\Inno Setup 6\ISCC" "../build/release/vst3d/installer-vst3.iss"
 EXIT /B 0
 
 :CMAKE
